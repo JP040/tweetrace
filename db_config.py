4d0c71390ae09e3b7af568ad6c767c2b3680edcb
JP040 <jan.petzening@web.de>
2021-04-26 17:38:13 +0200

    Config files added

---
+++ db_config.py
@@ -0,0 +1,10 @@
+"""This file is used to store the config parameters for the database access.
+   You will need to fill in the respective config details of the database
+   you plan to use.
+"""
+
+host = 'xyz'
+port = 'xyz'
+username = 'xyz'
+password = 'xyz' 
+db = 'xyz'
---
+++ twitter_config.py
@@ -0,0 +1,12 @@
+"""This file is used to store the access details for the Twitter API access.
+   You will need to fill in your own access details that you receive after
+   successfully applying for Twitter API access.
+"""
+
+Twitter = {
+    "consumer_key":"xyz",
+    "consumer_secret":"xyz",
+    "access_token":"xyz",
+    "access_token_secret":"xyz"
+}
+
